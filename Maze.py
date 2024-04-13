4207befb5af36dadaf98e66b7349844debeb94fb
Darren Chang <dchang@immersivefusion.com>
2024-04-13 01:02:58 -0400

    Created heuristic functions

--- Maze.py
+++ Maze.py
@@ -7,6 +7,7 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 import pygame
+import math
 
 class Maze():
 
@@ -23,7 +24,17 @@ class Maze():
         matrix[0, 0] = 'O'  # Guarantees start is open
         matrix[-1, -1] = 'G'    # Makes the bottom-right cell the goal
         return matrix
+    
+    # Manhattan distance heuristic function
+    def manhattan_distance(self, cur_row, cur_col, goal_row, goal_col):
+        return abs(cur_row - goal_row) + abs(cur_col - goal_col)
+    
+    # Euclidean distance heuristic function
+    def euclidean_distance(self, cur_row, cur_col, goal_row, goal_col):
+        return math.sqrt((cur_row - goal_row)**2 + (cur_col - goal_col)**2)
 
 # TESTING CODE
 maze = Maze(10)
 print(maze.maze)
+print(maze.manhattan_distance(0, 0, 3, 7))
+print(maze.euclidean_distance(0, 0, 10, 10))
